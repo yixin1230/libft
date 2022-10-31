6a43fe56b8a02a5113f1cb3c45576b2089b68b96
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2022-10-31 17:51:57 +0100

    list

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/31 17:25:23 by yizhang       #+#    #+#                 */
-/*   Updated: 2022/10/31 17:45:57 by yizhang       ########   odam.nl         */
+/*   Updated: 2022/10/31 17:51:01 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 t_list *ft_lstnew(void *content)
 {
-    t_list  node;
+    t_list  *node;
 
-    node->content = content;
+    node = malloc(sizeof(t_list));
+    node->content = (t_list *)content;
     node->next = NULL;
     return (node);
 }
